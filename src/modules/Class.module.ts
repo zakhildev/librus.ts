bf3ed0da2dca51d8d5a2ec6653f41fbafef9d120
Piotr Blecharski <piotr.blecharski02@gmail.com>
2022-10-11 19:21:46 +0200

    Prepare Class module

--- src/modules/Class.module.ts
+++ src/modules/Class.module.ts
@@ -1,8 +1,5 @@
 import Module from '../lib/Module';
-import Settings from '../lib/Settings';
 
 export default class Class extends Module {
-  public async getMe() {
-    const res = await this.librus.api.get(Settings.apiUrl + 'Me');
-  }
+  //  TODO: Handle class endpoint
 }
